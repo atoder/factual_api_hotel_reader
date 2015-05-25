c749f794bb586eab18f95f7e1631c23f040c75f6
atoder <alexandert@gmail.com>
2015-05-24 20:02:32 -0700

    Initial api set up

---
+++ test.rb
@@ -0,0 +1,38 @@
+F_KEY = "MMWKyQISyex0ODYyxpgjSefn5xzfeGV6ssWtWJZq"
+F_SECRET = "Yor44nLoueNMlXXRQ1cKm1TjeZZF0b22LdByMtXb"
+
+## If specific gem is not installed, # this function will install it on the machine
+def load_gem(name, version=nil)
+  # needed if your ruby version is less than 1.9
+  require 'rubygems'
+
+  begin
+    gem name, version
+  rescue LoadError
+    version = "--version '#{version}'" unless version.nil?
+    system("gem install #{name} #{version}")
+    Gem.clear_paths
+    retry
+  end
+
+  # Load proper gems
+  # Special require case for factual gem
+  require 'factual' if name == 'factual-api'
+  require name if name != 'factual-api'
+
+end
+
+# load all needed gems
+#load_gem 'httparty'
+load_gem 'factual-api'
+
+
+factual = Factual.new(F_KEY, F_SECRET)
+#info = factual.table("hotels-us").select("factual_id", "name", "locality", "region", "postcode", "country").limit(5)
+#puts info.rows
+
+info = factual.table("hotels-us").select("factual_id", "name", "locality", "region", "postcode", "country").include_count("true")
+puts info.total_count
+
+
+
