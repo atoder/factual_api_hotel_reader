468cbd8cf26c7689150fc902a9de1231ee84e234
atoder <alexandert@gmail.com>
2015-05-24 20:10:59 -0700

    Initial api set up

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 F_KEY = "MMWKyQISyex0ODYyxpgjSefn5xzfeGV6ssWtWJZq"
 F_SECRET = "Yor44nLoueNMlXXRQ1cKm1TjeZZF0b22LdByMtXb"
 
-## If specific gem is not installed, # this function will install it on the machine
+# If specific gem is not installed, # this function will install it on the machine
 def load_gem(name, version=nil)
   # needed if your ruby version is less than 1.9
   require 'rubygems'
@@ -28,11 +28,14 @@ load_gem 'factual-api'
 
 
 factual = Factual.new(F_KEY, F_SECRET)
+
+# Get total count
+info = factual.table("hotels-us").select("factual_id", "name", "locality", "region", "postcode", "country").include_count("true")
+total_hotels = info.total_count
+p total_hotels
+
+
+
 #info = factual.table("hotels-us").select("factual_id", "name", "locality", "region", "postcode", "country").limit(5)
 #puts info.rows
-
-info = factual.table("hotels-us").select("factual_id", "name", "locality", "region", "postcode", "country").include_count("true")
-puts info.total_count
-
-
 
